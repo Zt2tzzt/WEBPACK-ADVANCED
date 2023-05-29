2490c00c83e2b1941a89063f38727cef28024eeb
ZeT1an <584633819@qq.com>
2023-05-29 22:41:44 +0800

    chore: remove cli-project;

--- demo-project/CLIProject/lib/config/repo-constant.js
+++
@@ -1,5 +0,0 @@
-const VUE_REPO = 'direct:https://gitee.com/Zt2tzzt/vue3_template.git#main'
-
-module.exports = {
-  VUE_REPO
-}
--- demo-project/CLIProject/lib/utils/write-file.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-
-function writeFile(path, content) {
-  return fs.promises.writeFile(path, content)
-}
-
-module.exports = writeFile
